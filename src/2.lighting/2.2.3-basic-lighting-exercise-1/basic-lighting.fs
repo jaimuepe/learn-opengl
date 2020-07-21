f2ff84282752c3f3a3c82355aef481123429db8d
Jaime M <jaimuepe@gmail.com>
2020-07-21 23:59:22 +0200

    basic lighting - exercise 1

---
+++ src/2.lighting/2.2.3-basic-lighting-exercise-1/basic-lighting.fs
@@ -0,0 +1,47 @@
+#version 450 core
+
+in vec3 FragPos;
+in vec3 Normal;
+
+out vec4 FragColor;
+
+uniform vec3 objectColor;
+
+uniform vec3 lightPos;
+uniform vec3 lightColor;
+
+uniform vec3 viewPos;
+
+void main() {
+
+  // ambient light
+  float ambientStrength = 0.2;
+  vec3 ambient = ambientStrength * lightColor;
+
+  // diffuse light
+
+  vec3 n = normalize(Normal);
+
+  // direction fragment->light
+  vec3 lightDir = normalize(lightPos - FragPos);
+
+  float diff = max(0.0, dot(n, lightDir));
+  vec3 diffuse = diff * lightColor;
+
+  // specular light
+
+  float specularStrength = 0.5;
+
+  // negative! we want light->fragment now to get fragment->light'
+  vec3 reflectDir = reflect(-lightDir, n);
+
+  // fragment->eye
+  vec3 viewDir = normalize(viewPos - FragPos);
+
+  float spec = pow(max(0.0, dot(reflectDir, viewDir)), 32);
+
+  vec3 specular = spec * lightColor;
+
+  vec3 result = (ambient + diffuse + specular) * objectColor;
+  FragColor = vec4(result, 1.0);
+}
